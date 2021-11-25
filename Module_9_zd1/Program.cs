076371fa9795e9e03fadd40c77bad32c893f5bae
Alyona Melekhova <78417928+Mona2200@users.noreply.github.com>
2021-11-25 21:45:55 +0300

    Add files via upload

---
+++ Module_9_zd1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Module_9_zd1
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Exception[] exceptions = new Exception[] { new DivideByZeroException(), new ArgumentNullException(), new NotImplementedException(), new NotSupportedException(), new MyException("Моё исключение") };
+         try
+         {
+            Console.WriteLine("1 - вызвать исключение DivideByZeroException\n2 - вызвать исключение ArgumentNullException\n3 - вызвать исключение NotImplementedException\n4 - вызвать исключение NotSupportedException\n5 - вызвать исключение MyException");
+            string n = Console.ReadLine();
+            switch (n)
+            {
+               case "1":
+                  throw new DivideByZeroException();
+               case "2":
+                  throw new ArgumentNullException();
+               case "3":
+                  throw new NotImplementedException();
+               case "4":
+                  throw new NotSupportedException();
+               case "5":
+                  throw new MyException();
+               default:
+                  Console.WriteLine("Исключения не вызваны");
+                  break;
+            }
+         }
+         catch (DivideByZeroException)
+         {
+            Console.WriteLine(exceptions[0].Message);
+         }
+         catch (ArgumentNullException)
+         {
+            Console.WriteLine(exceptions[1].Message);
+         }
+         catch (NotImplementedException)
+         {
+            Console.WriteLine(exceptions[2].Message);
+         }
+         catch (NotSupportedException)
+         {
+            Console.WriteLine(exceptions[3].Message);
+         }
+         catch (MyException)
+         {
+            Console.WriteLine(exceptions[4].Message);
+         }
+      }
+   }
+
+   public class MyException : Exception
+   {
+      public MyException()
+      { }
+      public MyException(string message) : base(message)
+      { }
+   }
+}
---
+++ Module_9_zd2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+namespace Module_9_zd2
+{
+
+   class Program
+   {
+
+      public static void Main(string[] args)
+      {
+
+         SortingSelection sorting = new SortingSelection();
+         sorting.EneteredEvent += SortintArray;
+         try
+         {
+            sorting.Read();
+         }
+         catch (FormatException ex)
+         {
+            Console.WriteLine("Вы ввели не цифру");
+         }
+         catch (MyException ex)
+         {
+            Console.WriteLine(ex.Message);
+         }
+      }
+      public static void SortintArray(int n)
+      {
+         string[] surnames = new string[] { "Павлов", "Иванов", "Петров", "Климов", "Юрченко" };
+         switch (n)
+         {
+            case 1:
+               Array.Sort(surnames);
+               break;
+            case 2:
+               IComparer revComparer = new ReverseComparer();
+               Array.Sort(surnames, revComparer);
+               break;
+            default:
+               throw new MyException();
+         }
+         foreach (var words in surnames)
+            Console.WriteLine(words);
+      }
+   }
+
+   public class SortingSelection
+   {
+      public delegate void EneteredDelegate(int n);
+      public event EneteredDelegate EneteredEvent;
+
+      public void Read()
+      {
+         Console.WriteLine("Введите 1, чтобы сделать прямую сортировку фамилий\nВведите 2, чтобы сделать обратную сортировку фамилий");
+         int n = Convert.ToInt32(Console.ReadLine());
+         if (n != 1 && n != 2) throw new MyException("Вы ввели неправильное число");
+         Entered(n);
+      }
+
+      protected virtual void Entered(int n)
+      {
+         EneteredEvent?.Invoke(n);
+      }
+   }
+
+   public class MyException : Exception
+   {
+      public MyException()
+      { }
+      public MyException(string message) : base(message)
+      { }
+   }
+
+   public class ReverseComparer : IComparer
+   {
+      public int Compare(Object x, Object y)
+      {
+         return (new CaseInsensitiveComparer()).Compare(y, x);
+      }
+   }
+}
